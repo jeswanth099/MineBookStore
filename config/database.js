da17e8a1351814324b57a712ed1d82ec125adefe
jeswanth099 <jeswanth.venkatesh@evanam.com>
2025-04-29 13:11:54 +0530

    Database connection

--- config/database.js
+++ config/database.js
@@ -1,8 +1,10 @@
 const { Sequelize } = require('sequelize');
 
+
 const sequelize = new Sequelize('bookstore_db', 'root', '12345678', {
     host: 'localhost',
     dialect: 'mysql'
 });
 
 module.exports = sequelize;
+
